d3ee122e652d3f62939b428e5617e395ae89c38b
EKA RESTU JUSTITIAN <ekarestujustitian@gmail.com>
2022-04-16 00:27:47 +0700

    Add files via upload

---
+++ Resources/checkin.Designer.vb
@@ -0,0 +1,335 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class checkin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.CustomerNameTextbox = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.CustomerIDTextbox = New System.Windows.Forms.TextBox()
+        Me.AddressTextbox = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.PhoneNumberTextBox = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.RoomClassTextbox = New System.Windows.Forms.TextBox()
+        Me.RoomPriceTextBox = New System.Windows.Forms.TextBox()
+        Me.StatusTextBox = New System.Windows.Forms.TextBox()
+        Me.RoomNumberComboBox = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.StayTimeComboBox = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Label1.Font = New System.Drawing.Font("ROG Fonts", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(539, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(842, 86)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "b-cheese hotel"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Label2.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(40, 200)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(348, 43)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Room Number"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Label4.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(40, 400)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(411, 43)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Customer Name"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Label5.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(40, 500)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(327, 43)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Customer ID"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Label6.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(40, 300)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(398, 43)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Stay Time (day)"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Label7.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(967, 200)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(312, 43)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Room Class"
+        '
+        'CustomerNameTextbox
+        '
+        Me.CustomerNameTextbox.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CustomerNameTextbox.Location = New System.Drawing.Point(495, 397)
+        Me.CustomerNameTextbox.Name = "CustomerNameTextbox"
+        Me.CustomerNameTextbox.Size = New System.Drawing.Size(1353, 51)
+        Me.CustomerNameTextbox.TabIndex = 12
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Label8.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(40, 600)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(239, 43)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "Address"
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ComboBox4.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Items.AddRange(New Object() {"YES", "NO"})
+        Me.ComboBox4.Location = New System.Drawing.Point(495, 799)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(453, 51)
+        Me.ComboBox4.TabIndex = 14
+        Me.ComboBox4.Text = "--Choose Here--"
+        '
+        'CustomerIDTextbox
+        '
+        Me.CustomerIDTextbox.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CustomerIDTextbox.Location = New System.Drawing.Point(495, 497)
+        Me.CustomerIDTextbox.Name = "CustomerIDTextbox"
+        Me.CustomerIDTextbox.Size = New System.Drawing.Size(1353, 51)
+        Me.CustomerIDTextbox.TabIndex = 15
+        '
+        'AddressTextbox
+        '
+        Me.AddressTextbox.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AddressTextbox.Location = New System.Drawing.Point(495, 597)
+        Me.AddressTextbox.Name = "AddressTextbox"
+        Me.AddressTextbox.Size = New System.Drawing.Size(1353, 51)
+        Me.AddressTextbox.TabIndex = 16
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Label9.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(40, 700)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(373, 43)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Phone Number"
+        '
+        'PhoneNumberTextBox
+        '
+        Me.PhoneNumberTextBox.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PhoneNumberTextBox.Location = New System.Drawing.Point(495, 697)
+        Me.PhoneNumberTextBox.Name = "PhoneNumberTextBox"
+        Me.PhoneNumberTextBox.Size = New System.Drawing.Size(1353, 51)
+        Me.PhoneNumberTextBox.TabIndex = 18
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Label10.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(40, 800)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(218, 43)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "Service"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(1614, 875)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(254, 90)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Next"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(1314, 875)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(254, 90)
+        Me.Button2.TabIndex = 21
+        Me.Button2.Text = "Cancel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'RoomClassTextbox
+        '
+        Me.RoomClassTextbox.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoomClassTextbox.Location = New System.Drawing.Point(1340, 197)
+        Me.RoomClassTextbox.Name = "RoomClassTextbox"
+        Me.RoomClassTextbox.ReadOnly = True
+        Me.RoomClassTextbox.Size = New System.Drawing.Size(508, 51)
+        Me.RoomClassTextbox.TabIndex = 24
+        '
+        'RoomPriceTextBox
+        '
+        Me.RoomPriceTextBox.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoomPriceTextBox.Location = New System.Drawing.Point(1340, 297)
+        Me.RoomPriceTextBox.Name = "RoomPriceTextBox"
+        Me.RoomPriceTextBox.ReadOnly = True
+        Me.RoomPriceTextBox.Size = New System.Drawing.Size(508, 51)
+        Me.RoomPriceTextBox.TabIndex = 25
+        '
+        'StatusTextBox
+        '
+        Me.StatusTextBox.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.StatusTextBox.Location = New System.Drawing.Point(1440, 397)
+        Me.StatusTextBox.Name = "StatusTextBox"
+        Me.StatusTextBox.Size = New System.Drawing.Size(408, 51)
+        Me.StatusTextBox.TabIndex = 26
+        Me.StatusTextBox.Visible = False
+        '
+        'RoomNumberComboBox
+        '
+        Me.RoomNumberComboBox.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.RoomNumberComboBox.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoomNumberComboBox.FormattingEnabled = True
+        Me.RoomNumberComboBox.Items.AddRange(New Object() {"101", "102", "103", "104", "105", "106", "107", "108"})
+        Me.RoomNumberComboBox.Location = New System.Drawing.Point(495, 197)
+        Me.RoomNumberComboBox.Name = "RoomNumberComboBox"
+        Me.RoomNumberComboBox.Size = New System.Drawing.Size(408, 51)
+        Me.RoomNumberComboBox.Sorted = True
+        Me.RoomNumberComboBox.TabIndex = 27
+        Me.RoomNumberComboBox.Text = "  Choose Room"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Label3.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(967, 300)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(296, 43)
+        Me.Label3.TabIndex = 28
+        Me.Label3.Text = "Room Price"
+        '
+        'StayTimeComboBox
+        '
+        Me.StayTimeComboBox.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.StayTimeComboBox.Font = New System.Drawing.Font("ROG Fonts", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.StayTimeComboBox.FormattingEnabled = True
+        Me.StayTimeComboBox.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7"})
+        Me.StayTimeComboBox.Location = New System.Drawing.Point(495, 297)
+        Me.StayTimeComboBox.Name = "StayTimeComboBox"
+        Me.StayTimeComboBox.Size = New System.Drawing.Size(408, 51)
+        Me.StayTimeComboBox.TabIndex = 29
+        Me.StayTimeComboBox.Text = "  Choose Day"
+        '
+        'checkin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.BackgroundImage = Global.PKL_B_Cheese.My.Resources.Resources._0_20200128_interiors_732_mrj
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1898, 1024)
+        Me.Controls.Add(Me.StayTimeComboBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.RoomNumberComboBox)
+        Me.Controls.Add(Me.StatusTextBox)
+        Me.Controls.Add(Me.RoomPriceTextBox)
+        Me.Controls.Add(Me.RoomClassTextbox)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.PhoneNumberTextBox)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.AddressTextbox)
+        Me.Controls.Add(Me.CustomerIDTextbox)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.CustomerNameTextbox)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "checkin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "checkin"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox4 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents CustomerNameTextbox As System.Windows.Forms.TextBox
+    Friend WithEvents CustomerIDTextbox As System.Windows.Forms.TextBox
+    Friend WithEvents AddressTextbox As System.Windows.Forms.TextBox
+    Friend WithEvents PhoneNumberTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents RoomClassTextbox As TextBox
+    Friend WithEvents RoomPriceTextBox As TextBox
+    Friend WithEvents StatusTextBox As TextBox
+    Friend WithEvents RoomNumberComboBox As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents StayTimeComboBox As ComboBox
+End Class
---
+++ Resources/checkin.vb
@@ -0,0 +1,110 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class checkin
+    Dim connection As MySqlConnection
+    Dim command As MySqlCommand
+    Dim roomprice As Integer
+    Dim roomxtimeprice As Integer
+
+    Private Sub RoomNumberComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles RoomNumberComboBox.SelectedIndexChanged
+        If (RoomNumberComboBox.SelectedItem = 101) Then
+            StatusTextBox.ResetText()
+            RoomClassTextbox.ResetText()
+
+            StatusTextBox.Text = "BOOK".ToString
+            RoomClassTextbox.Text = "Royal Room".ToString
+            roomprice = 2654000
+        ElseIf (RoomNumberComboBox.SelectedItem = 102) Then
+            StatusTextBox.ResetText()
+            RoomClassTextbox.ResetText()
+
+            StatusTextBox.Text = "BOOK".ToString
+            RoomClassTextbox.Text = "Royal Room".ToString
+            roomprice = 2654000
+
+        ElseIf (RoomNumberComboBox.SelectedItem = 103) Then
+            StatusTextBox.ResetText()
+            RoomClassTextbox.ResetText()
+
+            StatusTextBox.Text = "BOOK".ToString
+            RoomClassTextbox.Text = "Luxury Room".ToString
+            roomprice = 1340000
+
+        ElseIf (RoomNumberComboBox.SelectedItem = 104) Then
+            StatusTextBox.ResetText()
+            RoomClassTextbox.ResetText()
+
+            StatusTextBox.Text = "BOOK".ToString
+            RoomClassTextbox.Text = "Luxury Room".ToString
+            roomprice = 1340000
+
+        ElseIf (RoomNumberComboBox.SelectedItem = 105) Then
+            StatusTextBox.ResetText()
+            RoomClassTextbox.ResetText()
+
+            StatusTextBox.Text = "BOOK".ToString
+            RoomClassTextbox.Text = "Luxury Room".ToString
+            roomprice = 1340000
+
+        Else
+            StatusTextBox.ResetText()
+            RoomClassTextbox.ResetText()
+
+            StatusTextBox.Text = "BOOK".ToString
+            RoomClassTextbox.Text = "Premium Room".ToString
+            roomprice = 764000
+        End If
+    End Sub
+
+    Private Sub StayTimeComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles StayTimeComboBox.SelectedIndexChanged
+        roomxtimeprice = StayTimeComboBox.SelectedItem * roomprice
+        RoomPriceTextBox.Text = roomxtimeprice.ToString
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        connection = New MySqlConnection
+        connection.ConnectionString =
+        "server=localhost;user id=root;password=;database=pkldb"
+        Dim reader As MySqlDataReader
+
+        Try
+            connection.Open()
+            Dim Query As String
+            Query = "UPDATE datakamar SET status='BOOK' WHERE roomnumber='" & RoomNumberComboBox.SelectedItem & "';
+                    INSERT INTO datakustomer(
+                    customername, customerid, roomnumber, staytime , address, phonenumber, totalprice,
+                    driverservice, rentalservice, tourguideservice
+                    ) VALUES (
+                    '" & CustomerNameTextbox.Text & "', '" & CustomerIDTextbox.Text & "',
+                    '" & RoomNumberComboBox.SelectedItem & "', '" & StayTimeComboBox.SelectedItem & "',
+                    '" & AddressTextbox.Text & "', '" & PhoneNumberTextBox.Text & "',
+                    '" & RoomPriceTextBox.Text & "','-', '-', '-'
+                    )"
+            command = New MySqlCommand(Query, connection)
+            reader = command.ExecuteReader
+            MessageBox.Show("Data telah tersimpan")
+            connection.Close()
+
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+        Finally
+            connection.Dispose()
+
+        End Try
+
+        Visible = False
+        Dim mainmenu As New mainmenu
+        mainmenu.Visible = True
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+
+        Visible = False
+        Dim mainmenu As New mainmenu
+        mainmenu.Visible = True
+    End Sub
+
+
+End Class
